56bb7b968b74cd503a2eb92f5b33710d7128cbb8
小室沙央里 <saori@komuroisagoousatonoMacBook-Air.local>
2022-06-08 09:46:21 +0900

    swift

---
+++ swift/MyPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+func FizzBuzz(n:Int) ->Array<String>{
+    var array1 = [String]()
+    for i in 1...n{
+        if(i%3 == 0 && i%5 == 0){
+            array1 += ["FizzBuzz"]
+        }else if(i%3 == 0){
+            array1 += ["Fizz"]
+        }else if(i%5 == 0){
+            array1 += ["Buzz"]
+        }else{
+            let iStr:String = String(i)
+            array1 += [iStr]
+        }
+    }
+    return array1
+}
+print(FizzBuzz(n:15))
+
