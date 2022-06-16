7dc792fb8b472e741b2b9eb4aeb2e3d386dbe2fb
小室沙央里 <saori@komuroisagoousatonoMacBook-Air.local>
2022-06-16 21:07:05 +0900

    contains

---
+++ swift/Q4.playground/Contents.swift
@@ -0,0 +1,34 @@
+func rowCheck(input:[String]) -> [String]{
+let row1:[Character] = ["q","w","e","r","t","y","u","i","o","p"]
+let row2:[Character] = ["a","s","d","f","g","h","j","k","l"]
+let row3:[Character] = ["z","x","c","v","b","n","m"]
+var array = [String]()
+
+for word in input{
+    var row1con = false
+    var row2con = false
+    var row3con = false
+for character in word{
+    if row1.contains(character){
+        row1con = true
+    }else if row2.contains(character){
+        row2con = true
+    }else if row3.contains(character){
+        row3con = true
+    }
+}
+   
+    if row1con == true && row2con == false && row3con == false{
+        array.append(word)
+    }
+    if row2con == true && row1con == false && row3con == false{
+        array.append(word)
+    }
+    if row3con == true && row1con == false && row2con == false{
+        array.append(word)
+    }
+    }
+    return(array)
+}
+
+print(rowCheck(input:["Hello","Alaska","Dad","Peace"]))
--- swift/bubble1.playground/Contents.swift
+++ swift/bubble1.playground/Contents.swift
@@ -4,7 +4,7 @@ func bubblesort(input:[Int]) -> [Int]{
     let n = arr.count
     for i in 0..<n-1{
         for j in 0..<n-i-1{
-            if arr[j] > arr[j+1]{
+            if arr[j] < arr[j+1]{
                 let temp = arr[j]
                 arr[j] = arr[j+1]
                 arr[j+1] = temp
